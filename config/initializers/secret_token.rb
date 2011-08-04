4c55327cae9a56ff20590034a7a21fdab0c32dca
Niklas Hofer <niklas+dev@lanpartei.de>
2011-08-04 13:17:51 +0200

    generate rails base app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Urlaub::Application.config.secret_token = 'cc7d8a925e7f94201d2e386dd3c8fa6450f1dc5586095f02dfe5479891c7b6c107671d29afe43b065300bfe19e14d79ca7a76d60e20f590a47d3c37245ba4e35'
